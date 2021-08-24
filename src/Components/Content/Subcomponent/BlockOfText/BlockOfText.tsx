ab68a07a8ca42b6000eb03cafa445a990e4836a4
Artur Zaczek <artur200212@interia.pl>
2021-08-24 14:30:37 +0200

    responsivnes in content

--- src/Components/Content/Subcomponent/BlockOfText/BlockOfText.tsx
+++ src/Components/Content/Subcomponent/BlockOfText/BlockOfText.tsx
@@ -1,7 +1,11 @@
 import egg from '../../../../img/desktop/image-transform.jpg'
+import eggMobile from '../../../../img/mobile/image-transform.jpg'
 import glass from '../../../../img/desktop/image-stand-out.jpg'
+import glassMobile from '../../../../img/mobile/image-stand-out.jpg'
 import graphicDesign from '../../../../img/desktop/image-graphic-design.jpg'
+import graphicDesignMoblie from '../../../../img/mobile/image-graphic-design.jpg'
 import photography from '../../../../img/desktop/image-photography.jpg'
+import photographyMobile from '../../../../img/mobile/image-photography.jpg'
 
 import './BlockOfText.scss'
 
@@ -18,7 +22,7 @@ const BlockOfText = () => {
                 </div>
             </div>
 
-            <img className="egg" src={egg} alt="egg" />
+            {window.innerWidth <= 425 ? <img className="egg" src={eggMobile} alt="egg" /> : <img className="egg" src={egg} alt="egg" />}
 
             <div className='standOut'>
                 <h2 className='standOutTitle'>Stand out to the right audience</h2>
@@ -32,10 +36,10 @@ const BlockOfText = () => {
 
             </div>
 
-            <img className="glass" src={glass} alt="glass" />
+            {window.innerWidth <= 425 ? <img className="glass" src={glassMobile} alt="glass" /> : <img className="glass" src={glass} alt="glass" />}
 
             <div className="graphicDesign">
-                <img className="graphicDesignImg" src={graphicDesign} alt="graphic Design" />
+                {window.innerWidth <= 425 ? <img className="graphicDesignImg" src={graphicDesignMoblie} alt="graphic Design" /> : <img className="graphicDesignImg" src={graphicDesign} alt="graphic Design" />}
                 <div className="graphicDesignContent">
                     <h2 className="graphicDesignHeader">Graphic Design</h2>
                     <p className="graphicDesignText">Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</p>
@@ -43,7 +47,7 @@ const BlockOfText = () => {
             </div>
 
             <div className="photography">
-                <img className="photographyImg" src={photography} alt="photography" />
+                {window.innerWidth <= 425 ? <img className="photographyImg" src={photographyMobile} alt="photography" /> : <img className="photographyImg" src={photography} alt="photography" />}
                 <div className="photographyContent">
                     <h2 className="photographyHeader">Photography</h2>
                     <p className="photographyText">Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
